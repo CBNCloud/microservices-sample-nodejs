38c2d41047526aa161540327579ed6f6cd59b157
dimaspry <dimas.priyo@cbncloud.co.id>
2019-02-07 17:08:15 +0700

    form javascript

---
+++ frontend/public/form.js
@@ -0,0 +1,18 @@
+		$('.edit').on('click',function(){
+
+		var row = $(this).parents('tr').children();
+		var name = $(row[0]).text();
+		var email = $(row[1]).text();
+		var address = $(row[2]).text();
+		var phone = $(row[3]).text();
+		var id = row.find('#id').val();
+		console.log(row.find('#id').val());
+
+		var form = $('#edit').find('form');
+		form.find("[name='name']").val(name);
+		form.find("[name='email']").val(email);
+		form.find("[name='address']").val(address);
+		form.find("[name='phone']").val(phone);
+		form.find("#id").val(id);
+
+	})
